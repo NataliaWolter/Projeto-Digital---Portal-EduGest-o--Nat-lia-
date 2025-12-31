000da5f32a1dee0b2e7c871cf5b21c0a0b2b57b7
Natália Wolter <nataliamaia55@outlook.com>
2025-12-31 16:17:43 -0300

    Tela de Login Inicial

--- vite-project/src/pages/Login.jsx
+++ vite-project/src/pages/Login.jsx
@@ -2,7 +2,7 @@ function Login() {
     return(
         <div>
             <h1>Portal EduGestão</h1>
-            <input type="text" placeholder="Usuário" />
+            <input type="text" placeholder="Usuário" /> 
             <br />
             <input type="password" placeholder="Senha" />
             <br />
