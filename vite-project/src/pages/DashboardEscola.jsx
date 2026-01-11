5920e33a9742aebf2a820c74302e27de067219bf
Natália Wolter <nataliamaia55@outlook.com>
2026-01-10 21:00:34 -0300

    dashboard

--- vite-project/src/pages/DashboardEscola.jsx
+++ vite-project/src/pages/DashboardEscola.jsx
@@ -1,8 +1,21 @@
+import "./dashboard.css";
+
 function DashboardEscola() {
   return (
-    <div>
+    <div className="dashboard">
       <h1>Minha Escola</h1>
-      <p>Dashboard da Escola</p>
+
+      <div className="cards-container">
+        <div className="card alunos">
+          <h3>Total de Alunos</h3>
+          <p>320</p>
+        </div>
+
+        <div className="card vagas">
+          <h3>Vagas Livres</h3>
+          <p>45</p>
+        </div>
+      </div>
     </div>
   );
 }
